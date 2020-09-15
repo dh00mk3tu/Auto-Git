fc550e7af112ffa11fb4b041f593e0edbc847b5e
dh00mk3tu <cyberhybird@hotmail.com>
2020-09-15 15:37:26 +0530

    This commit is made by Auto-Git 4

--- auto-git.sh
+++ auto-git.sh
@@ -1,6 +1,5 @@
 
 n=1
-echo $n
 echo "Auto Git"
 echo "This script is now monitoring this current directory"
 echo "Enter the update interval between each push (Eg. 1 = 1 min)"
@@ -9,7 +8,6 @@ while :
 
 do 
     if [ -d .git ]; then
-        echo $n  
         echo "This is the current Repo Status"
         git status
         git add .;
