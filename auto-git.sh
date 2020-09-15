f81e689dca6ce75629ce2af3f4303129b0148be0
dh00mk3tu <cyberhybird@hotmail.com>
2020-09-15 15:12:58 +0530

    This commit is made by Auto-Git

--- auto-git.sh
+++ auto-git.sh
@@ -1,7 +1,3 @@
-RED='\033[1;31m'
-GREEN='\033[1;32m'
-NOCOLOR='\033[0m'
-
 
 echo "Auto Git"
 echo "This script is now monitoring this current directory"
@@ -11,16 +7,15 @@ while :
 
 do 
     if [ -d .git ]; then
-        GREEN='\033[1;32m'
-        $n = 1
-        echo "${GREEN}This is the current Repo Status ${NOCOLOR}"
+        n = 1
+        echo "This is the current Repo Status"
         git status
         git add .;
         git commit -m "This commit is made by Auto-Git ${n}"
         git push
         echo "Commit ${n} Made"
     else
-        echo "${RED}Fatal! This is Not a Git Repo ${NOCOLOR}"
+        echo "Fatal! This is Not a Git Repo"
         git rev-parse --git-dir 2> /dev/null;
     fi;
     d=$(($inter * 60));
