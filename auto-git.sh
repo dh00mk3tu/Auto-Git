b2070790450e543a081cbc2ab6f63eabc94f4384
dh00mk3tu <cyberhybird@hotmail.com>
2020-09-15 15:24:39 +0530

    This commit is made by Auto-Git

--- auto-git.sh
+++ auto-git.sh
@@ -7,14 +7,14 @@ while :
 
 do 
     if [ -d .git ]; then
-        echo $N
+        echo "$N"
         echo "This is the current Repo Status"
         git status
         git add .;
         git commit -m "This commit is made by Auto-Git "${N}""
         git push
         echo "Commit "$N" Made"
-        $N=$(($N + 1));
+        $N=$(("$N" + 1));
     else
         echo "Fatal! This is Not a Git Repo"
         git rev-parse --git-dir 2> /dev/null;
